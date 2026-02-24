61be5c20a1a2792f1ef0d31e9b1a8c6fdcc9127d
Jahzeal <jahzealibeh3@gmail.com>
2026-02-24 18:09:33 +0100

    commit

--- apps/backend/src/modules/system/system.controller.ts
+++ apps/backend/src/modules/system/system.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SystemService } from './system.service';
-import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
-import { RolesGuard } from '../../guards/roles.guard';
-import { Roles } from '../../decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
 import { UserRole } from '../users/entities/user.entity';
 
 @Controller('admin/system')
