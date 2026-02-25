e6274f4af88088b8330b23149b499e86802d1a92
Azeem <bljazeem@gmail.com>
2026-02-25 15:34:10 +0100

    public visitor signup

--- apps/backend/src/database/data-source.ts
+++ apps/backend/src/database/data-source.ts
@@ -17,7 +17,7 @@ export const dataSourceOptions: DataSourceOptions = {
   entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
   migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
   synchronize: false,
-  migrationsRun: true,
+  migrationsRun: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
---
+++ apps/backend/src/modules/visitors/dto/visitor-signup.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+} from 'class-validator';
+
+export class VisitorSignupDto {
+    @ApiProperty({ example: 'John' })
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @ApiProperty({ example: 'Doe' })
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @ApiProperty({ example: 'john@example.com' })
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @ApiProperty({ example: '+234 801 234 5678' })
+    @IsString()
+    @IsNotEmpty()
+    phone: string;
+}
