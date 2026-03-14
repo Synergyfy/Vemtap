0df74bb6a480c992da0a725a794be74d3173466c
danny_nabzzy-admin <nabothdaniel35@gmail.com>
2026-03-14 16:05:33 +0100

    patch update

--- apps/VemTap/app/support-chat/[conversationId]/page.tsx
+++ apps/VemTap/app/support-chat/[conversationId]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useRef, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { useAuthStore } from '@/store/useAuthStore';
