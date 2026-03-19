991b9c504fc0a2f6867b7ec79986a25eca88732e
danny_nabzzy-admin <nabothdaniel35@gmail.com>
2026-03-19 09:45:10 +0100

    update

--- apps/VemTap/app/dashboard/engagement/experience/default-success/page.tsx
+++ apps/VemTap/app/dashboard/engagement/experience/default-success/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import UserFormSettingsPage from '../../user-form/page';
+import UserFormSettingsPage from '../../user-form/UserFormSettingsView';
 
 export default function UserExperienceDefaultSuccessPage() {
     return <UserFormSettingsPage mode="success" defaultPreviewTab="thank_you" />;
