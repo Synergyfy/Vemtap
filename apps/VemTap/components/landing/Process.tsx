02c62d9396fa4db554243cd9d11f01416f52fa01
Frankemsinwa <frankemesinwa@gmail.com>
2026-04-06 10:21:58 +0100

    fixed

--- apps/VemTap/components/landing/Process.tsx
+++ apps/VemTap/components/landing/Process.tsx
@@ -17,7 +17,7 @@ const steps = [
         heading: 'Capture customer info',
         desc: 'Getting customer details is easy. VemTap captures info in under 2 seconds without them needing to download any app.',
         img: 'https://images.unsplash.com/photo-1512428559087-560fa5ceab42?auto=format&fit=crop&q=80&w=800',
-        color: 'bg-primary-dark'
+        color: 'bg-gray-900'
     },
     {
         step: '3. The Growth',
